599b5b28acaa135fca0406e0da00179eb03ca75d
Dmytro_Litinskyi <litinskiupro100dima@gmail.com>
2025-11-11 05:32:41 +0100

    ArrayList was created and List interface methods were overrided

--- src/main/java/core/basesyntax/ArrayList.java
+++ src/main/java/core/basesyntax/ArrayList.java
@@ -1,48 +1,132 @@
 package core.basesyntax;
 
+import java.util.NoSuchElementException;
+
 public class ArrayList<T> implements List<T> {
+    public static final int BASE_CAPACITY = 10;
+    public static final int GROW_CAPACITY = 1;
+    private Object[] objectData;
+    private int size;
+
+    public ArrayList() {
+        objectData = new Object[BASE_CAPACITY];
+        size = 0;
+    }
+
     @Override
     public void add(T value) {
-
+        checkCapacityForAdd(1);
+        objectData[size] = value;
+        size++;
     }
 
     @Override
     public void add(T value, int index) {
-
+        indexValidationForAdd(index);
+        checkCapacityForAdd(1);
+        System.arraycopy(objectData, index, objectData, index + 1, size - index);
+        objectData[index] = value;
+        size++;
     }
 
     @Override
     public void addAll(List<T> list) {
-
+        if (list == null || list.isEmpty()) {
+            return;
+        }
+        checkCapacityForAdd(list.size());
+        for (int i = 0; i < list.size(); i++) {
+            T valueToCopy = (T) list.get(i);
+            objectData[size + i] = valueToCopy;
+        }
+        size = size + list.size();
     }
 
     @Override
     public T get(int index) {
-        return null;
+        indexValidation(index);
+        return (T) objectData[index];
     }
 
     @Override
     public void set(T value, int index) {
-
+        indexValidation(index);
+        objectData[index] = value;
     }
 
     @Override
     public T remove(int index) {
-        return null;
+        indexValidation(index);
+        T removedEl = (T) objectData[index];
+        for (int i = index + 1; i < size; i++) {
+            objectData[i - 1] = objectData[i];
+            objectData[i] = null;
+        }
+        size--;
+        return removedEl;
     }
 
     @Override
     public T remove(T element) {
-        return null;
+        if (findIndexOfElement(element) == -1) {
+            throw new NoSuchElementException("No such element in this array");
+        }
+        return remove(findIndexOfElement(element));
     }
 
     @Override
     public int size() {
-        return 0;
+        return size;
     }
 
     @Override
     public boolean isEmpty() {
-        return false;
+        return size == 0;
+    }
+
+    private void checkCapacityForAdd(int addCount) {
+        int requiredCapacity = size + addCount;
+        if (requiredCapacity <= objectData.length) {
+            return;
+        }
+        int oldCapacity = objectData.length;
+        int newCapacity = oldCapacity + (oldCapacity >> GROW_CAPACITY);
+        if (newCapacity < requiredCapacity) {
+            newCapacity = requiredCapacity;
+        }
+        Object[] newArray = new Object[newCapacity];
+        System.arraycopy(objectData, 0, newArray, 0, size);
+        objectData = newArray;
+    }
+
+    private void indexValidation(int index) {
+        if (index < 0 || index >= size) {
+            throw new ArrayListIndexOutOfBoundsException("Wrong index: " + index);
+        }
+    }
+
+    private void indexValidationForAdd(int index) {
+        if (index < 0 || index > size) {
+            throw new ArrayListIndexOutOfBoundsException("Index out of bounds for add: "
+                    + index + ", size: " + size);
+        }
+    }
+
+    private int findIndexOfElement(T element) {
+        if (element == null) {
+            for (int i = 0; i < size; i++) {
+                if (objectData[i] == null) {
+                    return i;
+                }
+            }
+        }
+        for (int i = 0; i < size; i++) {
+            assert element != null;
+            if (element.equals(objectData[i])) {
+                return i;
+            }
+        }
+
+        return -1;
     }
 }
